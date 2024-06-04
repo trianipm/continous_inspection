cb92c4057a960b24808caee79ddd7cc885ee3847
trianipm <127008970+trianipm@users.noreply.github.com>
2024-06-05 06:04:35 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,42 +1,21 @@
 package src.main.java;
 
 public class Driver {
+
 	public static void main(String[] args) {
+
 		Mahasiswa mahasiswa = new Mahasiswa();
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(3.80);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(2.50);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
 
-		// Additional repeated blocks to increase duplication
-		mahasiswa.setIpk(3.20);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(1.80);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(4.00);
 		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
 		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
 
 		mahasiswa.setIpk(3.80);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(2.50);
+
 		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
 		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
 
-		mahasiswa.setIpk(3.20);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(1.80);
-		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
-		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
-		mahasiswa.setIpk(4.00);
+		mahasiswa.setIpk(2.50);
+
 		System.out.println("IPK saat ini: " + mahasiswa.getIpk());
 		System.out.println("Status Kelulusan: " + mahasiswa.getStatusKelulusan());
 	}
